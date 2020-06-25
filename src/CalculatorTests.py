47f78e5432e7b67c493f978a8fb113fd6b731a2c
stephanieasabere <sa2222@njit.edu>
2020-06-24 23:56:44 -0400

    Feature: set up add,subtraction,multiplication,division, square, and squareroot

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -9,6 +9,43 @@ class MyTestCase(unittest.TestCase):
         calculator = Calculator()
         self.assertIsInstance(calculator,Calculator)
 
+    def test_calculator_addition(self):
+        calculator = Calculator()
+        result = calculator.addition(1, 2)
+        self.assertEqual(3, result)
+
+    def test_calculator_subrtraction(self):
+        calculator = Calculator()
+        result = calculator.subtraction(1, 2)
+        self.assertEqual(-1, result)
+
+    def test_calculator_multiplication(self):
+        calculator = Calculator()
+        result = calculator.multiplication(1, 2)
+        self.assertEqual(2, result)
+
+    def test_calculator_division(self):
+        calculator = Calculator()
+        result = calculator.division(4, 2)
+        self.assertEqual(2, result)
+
+    def test_calculator_square(self):
+        calculator = Calculator()
+        result = calculator.square(6)
+        self.assertEqual(36, result)
+
+    def test_calculator_squareroot(self):
+        calculator = Calculator()
+        result = calculator.squareroot(25)
+        self.assertEqual(5, result)
+
+    def test_calculator_addition(self):
+        calculator = Calculator()
+        result = calculator.addition(3, 2)
+        self.assertEqual(5, result)
+
+
+
 
 
 if __name__ == '__main__':
